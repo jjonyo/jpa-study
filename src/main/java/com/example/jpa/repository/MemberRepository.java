4b5d4b1535c6121b050e2fc6ce3ee92347430525
jjonyo <jonghyuk96@cau.ac.kr>
2022-04-08 01:09:51 +0900

    Member domain

---
+++ src/main/java/com/example/jpa/repository/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.domain.Member;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class MemberRepository {
+
+  @PersistenceContext
+  EntityManager em;
+
+  @Transactional
+  public Long save(Member member){
+    em.persist(member);
+    return member.getId();
+  }
+
+  public Member findById(Long id){
+    return em.find(Member.class, id);
+  }
+}
---
+++ src/test/java/com/example/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.jpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.jpa.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Test
+  void save() {
+    Member member = new Member();
+    member.setName("user2");
+
+    Long id = memberRepository.save(member);
+
+    Member savedMember = memberRepository.findById(id);
+
+    assertThat(savedMember.getName()).isEqualTo(member.getName());
+    assertThat(savedMember.getId()).isNotNull();
+  }
+}
